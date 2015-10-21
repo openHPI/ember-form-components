b3318029094c6925169f8df876e9131b5ca5fb58
Franz Liedke <franz.liedke@student.hpi.de>
2015-10-21 18:51:13 +0200

    Implement canceling edit mode when clicking elsewhere on the document

--- addon/components/editable-field.js
+++ addon/components/editable-field.js
@@ -29,5 +29,42 @@ export default Ember.Component.extend({
         this.get('onsave')();
       }
     }
+  },
+
+  /*
+   * CANCEL WHEN CLICKING OUTSIDE
+   * If we're in editing mode, a click on the outside (somewhere on the document)
+   * will cancel editing.
+   */
+
+  boundClickHandler: Ember.computed(function() {
+    return this.get('clickHandler').bind(this);
+  }),
+
+  clickHandler(e) {
+    // Only need to cancel if we're in editing mode
+    if (!this.get('isEditing')) { return; }
+
+    var element = this.get('element');
+    var $target = Ember.$(e.target);
+    var thisIsElement = $target.closest(element).length === 1;
+
+    // If the user clicked on the placeholder for editing, isEditing will already
+    // be true. However, the edit button will already be removed from the DOM, thus
+    // thisIsElement will be false. We therefore have to return early in this case.
+    if ($target.hasClass('internal--edit-button')) { return; }
+
+    // If we're not clicking on the element itself, cancel editing mode
+    if (!thisIsElement) {
+      this.set('isEditing', false);
+    }
+  },
+
+  didInsertElement() {
+    Ember.$(document).on('click', this.get('boundClickHandler'));
+  },
+
+  willDestroyElement() {
+    Ember.$(document).off('click', this.get('boundClickHandler'));
   }
 });
